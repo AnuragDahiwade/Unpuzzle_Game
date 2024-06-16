8290ef003d58e1a56c65dae070d03e44c794dcd9
Anurag Dahiwade <91058090+AnuragDahiwade@users.noreply.github.com>
2024-06-17 02:11:05 +0530

    Add files via upload

---
+++ src/scenes/Boot.js
@@ -0,0 +1,27 @@
+import { Scene } from 'phaser';
+import game from '../main.js';
+
+export class Boot extends Scene
+{
+    constructor ()
+    {
+        super('Boot');
+    }
+   
+    preload ()
+    {
+        this.cameras.main.setBackgroundColor(0xA020F0);
+
+        this.load.image('unpuzzle_game_logo', 'assets/unpuzzle_game_logo.png');
+    }
+
+    create ()
+    {
+        this.scene.start('Preloader');
+        // this.scene.get('GameTemplate').changeScene('Boot', 'Preloader');
+
+    }
+}
+
+
+export default Boot;
